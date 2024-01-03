3b3e9ab810f0144260ae8781044115216e5db2b5
Guslev Kirill <38893535+guslevkirill@users.noreply.github.com>
2024-01-03 21:16:52 +0000

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+tgToken = '6927008789:AAHCPMpUdX0WrQFXOkAVfL_dlWR-Up5VpzY'
+channel_url = 'https://t.me/+uFq1M1U18D9jZmUy'
+chat_id = '@+uFq1M1U18D9jZmUy'
+admin_ad = '332155717'
+
+wrong_words = ['хуй', 'хуй бычий', 'путин', 'украина', 'омерика', 'америка', 'сша', 'пидор', 'пидарас', 'лох']
